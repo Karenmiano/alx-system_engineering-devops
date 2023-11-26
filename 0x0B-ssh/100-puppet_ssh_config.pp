ad82e1d451a57e23c13eab42b355005275eeb7e9
Karen Miano <mianokaren1798@gmail.com>
2023-11-26 12:10:45 -0800

    puppet config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,14 +3,14 @@ include stdlib
 
 file_line { 'private_key _source':
   ensure  => present,
-  path    => '/root/.ssh/config',
+  path    => '/etc/ssh/ssh_config',
   line    => '  IdentityFile ~/.ssh/school',
   replace => true,
 }
 
 file_line { 'no passwords':
   ensure  => present,
-  path    => '/root/.ssh/config',
+  path    => '/etc/ssh/ssh_config',
   line    => '  PasswordAuthentication no',
   replace => true,
 }
