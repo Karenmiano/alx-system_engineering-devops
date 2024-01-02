b91e98e114e104b36bd92b5d4619c2226c6704dd
Karenmiano <mianokaren1798@gmail.com>
2024-01-02 14:20:00 +0300

    puppet on config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,14 +5,10 @@ file_line { 'no passwords':
   ensure   => present,
   path     => '/etc/ssh/ssh_config',
   line     => '    PasswordAuthentication no',
-  match    => '.*PasswordAuthentication',
-  multiple => 'true',
 }
 
 file_line { 'identity file':
   ensure   => present,
   path     => '/etc/ssh/ssh_config',
   line     => '    IdentityFile ~/.ssh/school',
-  match    => '.*IdentityFile',
-  multiple => 'true',
 }
