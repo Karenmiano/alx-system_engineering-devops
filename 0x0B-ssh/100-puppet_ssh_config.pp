5566bd76225664459295b1dc82f6292dfdcb2464
Karenmiano <mianokaren1798@gmail.com>
2024-01-02 13:05:15 +0300

    puppet on config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,16 @@
-file { '/home/karen/.ssh/config':
-  ensure  => file,
-  content => "Host *\n PasswordAuthentication no\n IdentityFile ~/.ssh/school",
+# Seting up my client config file
+include stdlib
+
+file_line { 'no passwords':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
