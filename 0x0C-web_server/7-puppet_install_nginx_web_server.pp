a6d7ed40a40b2dc47d0d362630c254a572735393
Karenmiano <mianokaren1798@gmail.com>
2024-01-08 15:47:50 +0300

    configure nginx by puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -35,7 +35,7 @@ server {
 	index index.html index.htm index.nginx-debian.html;
 
 	server_name _;
-    rewrite ^/redirect_me https://example.com permanent;
+    rewrite ^/redirect_me/ https://example.com permanent;
 
 	location / {
 		try_files $uri $uri/ =404;
