c7cf7a4c60a30a63f53a2bf1da845fe1c30e2496
Karenmiano <mianokaren1798@gmail.com>
2024-01-09 15:34:08 +0300

    puppet nginx on server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -35,7 +35,8 @@ server {
 	index index.html index.htm index.nginx-debian.html;
 
 	server_name _;
-    rewrite ^/redirect_me/ https://example.com permanent;
+    rewrite ^/redirect_me.*$ https://example.com permanent;
+
 
 	location / {
 		try_files $uri $uri/ =404;
@@ -46,4 +47,9 @@ EOF
 file { '/etc/nginx/sites-available/default':
     ensure  => 'file',
     content => $config,
+}
+
+exec { 'allow_nginx_http':
+  command     => '/usr/sbin/ufw allow Nginx\ HTTP',
+  path        => '/usr/sbin',
 }
