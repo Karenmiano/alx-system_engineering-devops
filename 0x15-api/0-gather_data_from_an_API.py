c431db36a06df708cde417d1972ec8ef08a9396f
Karenmiano <mianokaren1798@gmail.com>
2024-01-31 19:06:13 +0300

    retrieve from API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""retrieves data from the website https://jsonplaceholder.typicode.com
+using its exposed api, about a user and the tasks completed"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url1 = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
+    url2 = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos"
+    res1 = requests.get(url1)
+    res2 = requests.get(url2)
+    employee = res1.json()['name']
+    tasks = res2.json()
+    num_tasks = len(tasks)
+    done = 0
+    for task in tasks:
+        if task['completed'] is True:
+            done += 1
+    print(f'Employee {employee} is done with tasks ({done}/{num_tasks}):')
+    for task in tasks:
+        if task['completed'] is True:
+            title = task['title']
+            print(f'\t {title}')
