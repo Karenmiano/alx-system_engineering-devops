6e6b7011424f5cd8b609adbfdf12851a1a456b8f
Karenmiano <mianokaren1798@gmail.com>
2024-02-13 19:02:19 +0300

    subreddit subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,11 +10,13 @@ def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers for a given subreddit
     """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'python:subscriber.counter by Karen_miano'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
+        subscribers = data.get('data').get('subscribers')
         return subscribers
     return 0
