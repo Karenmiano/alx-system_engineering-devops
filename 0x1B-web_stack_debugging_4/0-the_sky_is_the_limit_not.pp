2d45691ccae0d9993833ef4503faab38cae3548b
Karenmiano <mianokaren1798@gmail.com>
2024-02-22 23:44:18 +0300

    user limits

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # correct file descriptor limit in stack
 exec {'change-limit':
-    provider     => 'shell',
+    provider => 'shell',
     command  => 'sed -i "s/ULIMIT=\"-n [0-9]\+\"/ULIMIT=\"-n 10000\"/g" /etc/default/nginx',
     before   => Exec['restart'],
 }
